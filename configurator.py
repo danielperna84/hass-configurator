996ec1570700cbfe5c0f027d0099999c70dde95e
Daniel Perna <danielperna84@gmail.com>
2017-02-13 00:47:43 +0100

    Fix for permission errors

--- configurator.py
+++ configurator.py
@@ -1610,9 +1610,13 @@ def get_dircontent(path):
         edata['dir'] = path
         edata['fullpath'] = os.path.abspath(os.path.join(path, e))
         edata['type'] = 'dir' if os.path.isdir(edata['fullpath']) else 'file'
-        stats = os.stat(os.path.join(path, e))
-        edata['size'] = stats.st_size
-        edata['modified'] = stats.st_mtime
+        try:
+            stats = os.stat(os.path.join(path, e))
+            edata['size'] = stats.st_size
+            edata['modified'] = stats.st_mtime
+        except Exception:
+            edata['size'] = 0
+            edata['modified'] = 0
         dircontent.append(edata)
 
     return dircontent
