5074a2c1c35f166160fac6460aeb3922d855fb44
JT Martinez <five1eight@gmail.com>
2017-08-03 11:23:43 -0600

    Fixed jquery cdn

--- configurator.py
+++ configurator.py
@@ -1895,7 +1895,7 @@ INDEX = Template(r"""<!DOCTYPE html>
 </main>
 <input type="hidden" id="fb_currentfile" value="" />
 <!-- Scripts -->
-<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
