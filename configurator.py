d14d4a492f32aeee24ec6ecf04966b4d92ef7bcf
Daniel Perna <danielperna84@gmail.com>
2019-02-23 20:25:49 +0100

    Bump version

--- configurator.py
+++ configurator.py
@@ -108,7 +108,7 @@ SO.setFormatter(
     logging.Formatter('%(levelname)s:%(asctime)s:%(name)s:%(message)s'))
 LOG.addHandler(SO)
 RELEASEURL = "https://api.github.com/repos/danielperna84/hass-configurator/releases/latest"
-VERSION = "0.3.4"
+VERSION = "0.3.5"
 BASEDIR = "."
 DEV = False
 LISTENPORT = None
